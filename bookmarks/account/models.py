b7256d8a66ec786fe2f99e12a41d7db4168c15a4
Michał <mwarioxin@gmail.com>
2021-04-08 20:25:02 +0200

    WIP - create edit user template

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -10,10 +10,10 @@ from django.conf import settings
 # The reverse side of the relationship is an implicit one-to-one relationship with the related model instead
 # of a manager for multiple elements. From each side of the relationship, you retrieve a single related object.
 
-
+# have all user filds plus given below (date_of_birth and photo)
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE) # delete profile whem user is deleted
+                                on_delete=models.CASCADE) # delete profile when user is deleted
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/',
                               blank=True)
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+
 
 # Create your views here.
 def user_login(request): # Custom login view
@@ -26,6 +28,7 @@ def user_login(request): # Custom login view
         form = LoginForm()
         return render(request, 'account/login.html', {'form': form})
 
+
 @login_required
 def dashboard(request):
     return render(request,
@@ -45,6 +48,8 @@ def register(request):
             )
             # Save user object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -53,3 +58,27 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        print(request.user)
+        print(request.POST)
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+
+        else:
+            user_form = UserEditForm(instance=request.user)
+            profile_form = ProfileEditForm(instance=request.user.profile)
+
+        return render(request,
+                      'account/edit.html',
+                      {'user_form': user_form,
+                       'profile_form': profile_form})
