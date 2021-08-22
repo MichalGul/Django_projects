d50e37dde97bfe6a134efd122ea43958d8a54d92
MG <michal.gulczynski93@gmail.com>
2021-08-22 13:37:45 +0200

    Added clean url to images form

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -6,6 +6,14 @@ class ImageCreateForm(forms.ModelForm):
         model = Image
         fields = ('title', 'url', 'description')
         widgets = {
-            'url': forms.HiddenInput,
+            'url': forms.HiddenInput, # This widget is rendered as an HTML input element with a type="hidden" attribute
         }
 
+    # This method is executed for each field when call is_valid()
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['.jpg', '.jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extension')
+        return url
