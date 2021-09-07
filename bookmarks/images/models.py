1b71a713458e8ec2129db1a06c842778d777d3b7
Michał <mwarioxin@gmail.com>
2021-09-07 15:12:04 +0200

    Created image detail view

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -22,12 +23,15 @@ class Image(models.Model):
                                         related_name='images_liked',
                                         blank = True)
 
-def __str__(self):
-    return self.title
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
 
-# Override the save() method of the Image model to automatically generate the slug field based on the value of the title field
-def save(self, *args, **kwargs):
-    if not self.slug:
-        self.slug = slugify(self.title)
-    super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+    # Override the save() method of the Image model to automatically generate the slug field based on the value of the title field
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
