4ffb399ff2363df523a32089e22eec1ef032c8f7
Michał <mwarioxin@gmail.com>
2021-09-07 11:35:46 +0200

    Created images form save method to download image from url

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
@@ -17,3 +20,23 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extension')
         return url
+
+    # override ModelForm save method to download image from given url (commit responsible for putting date to database)
+    def save(self, force_insert=False, force_update=False, commit=True):
+        # create a new image empty (model) instance
+        image = super().save(commit=False)
+        # get url data from form
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # download image from the given URL and call save of image  method of the image field,
+        # passing it a ContentFile object that is instantiated with the downloaded file content.
+        # In this way, you save the file to the media directory of your project.
+        # You pass the save=False parameter to avoid saving the object to the database yet
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()),
+                         save=False)
+        if commit: # save to database only when commit is set to true
+            image.save()
+        return image
