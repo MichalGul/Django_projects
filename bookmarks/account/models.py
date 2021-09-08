b63eb64a886bec812865bd91a64ea06f90655175
Michał <mwarioxin@gmail.com>
2021-09-08 15:11:41 +0200

    Created backend functionality to follow users, Made migrations for Contact model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,6 +1,11 @@
-from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
 # Create your models here.
+
+
 
 # However, the user model comes with very basic fields.
 # You may wish to extend it to include additional data.
@@ -19,3 +24,32 @@ class Profile(models.Model):
                               blank=True)
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+class Contact(models.Model):
+    # User who creates the relationship
+    user_from = models.ForeignKey('auth.User',
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    # User being followed
+    user_to = models.ForeignKey('auth.User',
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    # when the relation was created
+    created = models.DateTimeField(auto_now=True,
+                                   db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
+# Add following field to User dynamically todo change this to be in Profile model
+user_model = get_user_model() #retrive User model
+user_model.add_to_class('following', # add_to_class() method of Django models to monkey patch the User model
+                        models.ManyToManyField('self',
+                                               through=Contact,
+                                               related_name='followers',
+                                               symmetrical=False))
