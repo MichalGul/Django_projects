a7a65d4e18053f82f13065ef6ee033118400fba9
Michał <mwarioxin@gmail.com>
2021-09-10 13:54:35 +0200

    Created basic category and Products models with admin models.py Created templates for display list and detail products Created views handling getting images, sorting, displaying by category, etc. Importet css styles

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_shop',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -120,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# base URL that serves media files uploaded by users
+MEDIA_URL = '/media/'
+# local path where these files reside, which you build by dynamically prepending the BASE_DIR variable
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    # fields that can be edit in admin list view
+    list_editable = ['price', 'available']
+    # use the prepopulated_fields attribute to specify fields where the value is automatically set using
+    # the value of other fields. As you have seen before, this is convenient for generating slugs.
+    prepopulated_fields = {'slug': ('name',)}
