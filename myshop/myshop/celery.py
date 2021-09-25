93f4edd241b27c8d5cfc703aee5c0f79eac1b590
MG <michal.gulczynski93@gmail.com>
2021-09-25 21:19:43 +0200

    Created order app and order mechanism Used Celery to run taskts asynchronously Created view and forms to order items

---
+++ myshop/myshop/celery.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+app = Celery('myshop')
+# load every celery related settings from main settings.py with CELERY_prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+# auto-discover asynchronous tasks for your applications. Celery will look
+# for a tasks.py file in each application directory of applications added to
+# INSTALLED_APPS in order to load asynchronous tasks defined in it.
+app.autodiscover_tasks()
--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Application definition
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
